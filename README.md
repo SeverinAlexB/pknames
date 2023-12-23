845f02f84c6ec0831b271284f4d4bbb76a5ae059
Severin Buhler <sevi_buehler@hotmail.com>
2023-12-23 09:42:57 +0100

    added visualization boilerplate

---
+++ README.md
@@ -0,0 +1,15 @@
+# fancyd-rust
+
+An experimental and general Web of Trust (WoT) library. WIP.
+
+## Todos
+
+- [x] Simple probability predictions
+- [x] Backpropagation to blame intermediate for the right/wrong result.
+- [ ] Graph visualization
+- [ ] Graph pruning
+- [ ] Transform cyclical graph to acyclical.
+- [ ] CLI for a petname example use case.
+- [ ] Reseach gameability
+    - [ ] Graph cycle removal abuse
+    - [ ] Distrust abuse
--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,85 @@
-mod web_of_trust;
-mod cli;
+// mod web_of_trust;
+// mod cli;
 
-use web_of_trust::prediction::{node::WotNodeType, graph::WotGraph, predictor::WotPredictor};
+// fn main() {
+    // run_cli();
+// }
 
-use crate::{web_of_trust::prediction::node::{WotFollow, WotNode}, cli::cli::run_cli};
 
+use eframe::{run_native, App, CreationContext};
+use egui::Context;
+use egui_graphs::{
+    DefaultEdgeShape, DefaultNodeShape, Graph, GraphView, SettingsInteraction, SettingsStyle,
+};
+use petgraph::stable_graph::StableGraph;
+
+
+#[derive(Debug, Clone)]
+pub struct NodeProps {
+    pub label: String
+}
+
+pub struct InteractiveApp {
+    g: Graph<NodeProps, ()>,
+}
+
+impl InteractiveApp {
+    fn new(_: &CreationContext<'_>) -> Self {
+        let g = generate_graph();
+        Self { g }
+    }
+}
+
+
+impl App for InteractiveApp {
+    fn update(&mut self, ctx: &Context, _: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            let interaction_settings = &SettingsInteraction::new()
+                .with_dragging_enabled(true)
+                .with_node_clicking_enabled(true)
+                .with_node_selection_enabled(true)
+                .with_node_selection_multi_enabled(true)
+                .with_edge_clicking_enabled(true)
+                .with_edge_selection_enabled(true)
+                .with_edge_selection_multi_enabled(true);
+            let style_settings = &SettingsStyle::new().with_labels_always(true);
+            ui.add(
+                &mut GraphView::<_, _, _, _, DefaultNodeShape, DefaultEdgeShape>::new(&mut self.g)
+                    .with_styles(style_settings)
+                    .with_interactions(interaction_settings),
+            );
+        });
+    }
+}
+
+fn generate_graph() -> Graph<NodeProps, ()> {
+    let mut g = StableGraph::new();
+
+    let a = g.add_node(NodeProps{label: "A".to_string()});
+    let b = g.add_node(NodeProps{label: "B".to_string()});
+    let c = g.add_node(NodeProps{label: "C".to_string()});
+    
+
+    g.add_edge(a, b, ());
+    g.add_edge(a, b, ());
+    g.add_edge(b, c, ());
+    g.add_edge(c, a, ());
+
+    let mut graph = Graph::from(&g);
+    let indexes: Vec<petgraph::prelude::NodeIndex> = graph.nodes_iter().map(|(index, _)| index).collect();
+    for index in indexes.iter() {
+        let label = graph.node_mut(*index).unwrap().payload().label.clone();
+        graph.node_mut(*index).unwrap().set_label(label);
+    }
+    graph
+}
 
 fn main() {
-    run_cli();
-
-    // let mut nodes: Vec<WotNode> = Vec::new();
-
-    // // Classes
-    // nodes.push(WotNode {
-    //     pubkey: "d1".to_string(),
-    //     alias: Some(String::from("example.com1")),
-    //     typ: WotNodeType::WotClass,
-    // });
-    // nodes.push(WotNode {
-    //     pubkey: "d2".to_string(),
-    //     alias: Some(String::from("example.com2")),
-    //     typ: WotNodeType::WotClass,
-    // });
-
-    // nodes.push(WotNode {
-    //     pubkey: "n2".to_string(),
-    //     alias: None,
-    //     typ: WotNodeType::WotFollowNode {
-    //         follows: vec![
-    //             WotFollow::new("n2".to_string(), "d1".to_string(), 1.0).unwrap(),
-    //             WotFollow::new("n2".to_string(), "d2".to_string(), -1.0).unwrap()
-    //         ],
-    //     },
-    // });
-
-    // nodes.push(WotNode {
-    //     pubkey: "n1".to_string(),
-    //     alias: None,
-    //     typ: WotNodeType::WotFollowNode {
-    //         follows: vec![
-    //             WotFollow::new("n1".to_string(), "d1".to_string(), -0.5).unwrap(),
-    //             WotFollow::new("n1".to_string(), "d2".to_string(), 0.0).unwrap()
-    //         ],
-    //     },
-    // });
-
-    // nodes.push(WotNode {
-    //     pubkey: "me".to_string(),
-    //     alias: None,
-    //     typ: WotNodeType::WotFollowNode {
-    //         follows: vec![
-    //             WotFollow::new("me".to_string(), "n1".to_string(), 1.0).unwrap(),
-    //             WotFollow::new("me".to_string(), "n2".to_string(), 0.5).unwrap()
-    //         ],
-    //     },
-    // });
-
-    // let graph = WotGraph::new(nodes).unwrap();
-
-    // let predictor: WotPredictor = graph.into();
-    // let result = predictor.predict();
-    // println!("Result {}", result);
-
-
+    let native_options = eframe::NativeOptions::default();
+    run_native(
+        "egui_graphs_interactive_demo",
+        native_options,
+        Box::new(|cc| Box::new(InteractiveApp::new(cc))),
+    )
+    .unwrap();
 }
